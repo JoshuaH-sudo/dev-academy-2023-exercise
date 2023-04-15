4f95336dc367e5d0fc2714586279c952c9282dea
ElQwerto110100100 <joshyboy111@protonmail.com>
2023-04-15 14:32:36 +1000

    tar ball the station data the build can take place within the docker container

--- build/scripts/extract_datasets.sh
+++ build/scripts/extract_datasets.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-FILE=""
-DATASETS="$PWD/datasets"
-DIR="$DATASETS/journeys"
+DATASETS_PATH="$PWD/datasets"
+JOURNEY_DIR="$JOURNEY_DATASETS/journeys"
 
-if [ "$(ls -A $DIR)" ]; then
+if [ "$(ls -A $JOURNEY_DIR)" ]; then
   echo "Jounrey datasets already exists, skipping extraction"
 else
-  echo "$DIR is Empty, extracting journey datasets"
-  tar xvf $DATASETS/journey_data_sets.tar.bz2 -C .
+  echo "$JOURNEY_DIR is Empty, extracting journey datasets"
+  tar xvf $DATASETS_PATH/journey_datasets.tar.bz2 -C .
+fi
+
+STATIONS_DIR="$STATIONS_DATASETS/stations"
+if [ "$(ls -A $STATIONS_DIR)" ]; then
+  echo "Stations datasets already exists, skipping extraction"
+else
+  echo "$STATIONS_DIR is Empty, extracting journey datasets"
+  tar xvf $DATASETS_PATH/station_datasets.tar.bz2 -C .
 fi
