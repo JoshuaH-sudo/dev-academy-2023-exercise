34b5605dab7e7b32641e9b0dfbea01941609eb65
JoshuaH-sudo <JHwork101@protonmail.com>
2023-04-19 17:40:49 +1000

    improve build script to extract data after container setup.

--- scripts/extract_datasets.sh
+++ scripts/extract_datasets.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 DATASETS_PATH="$PWD/datasets"
-JOURNEY_DIR="$DATASETS_PATH/journeys"
 
+JOURNEY_DIR="$DATASETS_PATH/journeys"
+# Create the directory if it doesn't exist
+mkdir -p $JOURNEY_DIR
+# Check if the directory is empty
 if [ "$(ls -A $JOURNEY_DIR)" ]; then
   echo "Jounrey datasets already exists, skipping extraction"
 else
   echo "$JOURNEY_DIR is Empty, extracting journey datasets"
+
   tar xvf $DATASETS_PATH/journey_datasets.tar.bz2 -C .
 fi
 
 STATIONS_DIR="$DATASETS_PATH/stations"
+
+mkdir -p $STATIONS_DIR
 if [ "$(ls -A $STATIONS_DIR)" ]; then
   echo "Station datasets already exists, skipping extraction"
 else
   echo "$STATIONS_DIR is Empty, extracting station datasets"
+
   tar xvf $DATASETS_PATH/station_datasets.tar.bz2 -C .
 fi
