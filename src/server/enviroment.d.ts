6ba292f120ca4ce8e7200a424797cdae88940591
JoshuaH-sudo <JHwork101@protonmail.com>
2023-04-22 09:01:03 +1000

    update enviroment type to handle test node.env

---
+++ src/server/enviroment.d.ts
@@ -0,0 +1,11 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test';
+      MONGO_URI: string;
+      PORT?: string;
+    }
+  }
+}
+
+export {}
