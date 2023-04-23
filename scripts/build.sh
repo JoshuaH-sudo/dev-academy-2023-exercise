2e3a5fa9561840b5f7f77f0cd28c6a3ca880a1f2
JoshuaH-sudo <JHwork101@protonmail.com>
2023-04-23 11:51:50 +1000

    separate dockerfile to dev and prod build processes and update docker build to bundle client code inside dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,4 +14,7 @@ elif [ "$NODE_ENV" == "production" ]; then
   yarn webpack --config ./scripts/webpack/webpack.prod.js --mode production
 else
   echo 'No build mode selected set the NODE_ENV variable to either "development" or "production"'
+  exit 1
 fi
+
+sleep 4
