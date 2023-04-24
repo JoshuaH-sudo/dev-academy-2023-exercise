5cfe0910036800640687241e27d99c242d07d9a9
JoshuaH-sudo <JHwork101@protonmail.com>
2023-04-25 08:27:24 +1000

    extracting mongo_uri from file instead of env variable

--- src/server/enviroment.d.ts
+++ src/server/enviroment.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test';
       MONGO_URI?: string;
+      MONGO_URI_FILE?: string;
       PORT?: string;
     }
   }
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,8 @@ import path from "path"
 import cookieParser from "cookie-parser"
 import logger from "morgan"
 import mongoose from "mongoose"
-import expressStaticGzip from "express-static-gzip";
+import expressStaticGzip from "express-static-gzip"
+import fs from "fs"
 
 import app_router from "./routes/app"
 import journey_router from "./routes/journey"
@@ -20,8 +21,8 @@ import {
   clear_stations,
   import_stations_csv_to_database,
 } from "./controllers/station"
-const { config } = require('dotenv');
-config();
+const { config } = require("dotenv")
+config()
 
 const debugLog = debug("app:server:log")
 const errorLog = debug("app:server:error")
@@ -34,9 +35,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "../../", "public")))
-app.use("/files", expressStaticGzip(path.join(__dirname, "../../", "public"), {
- enableBrotli: true
-}));
+app.use(
+  "/files",
+  expressStaticGzip(path.join(__dirname, "../../", "public"), {
+    enableBrotli: true,
+  })
+)
 
 app.use("/", app_router)
 app.use("/journeys", journey_router)
@@ -48,12 +52,11 @@ app.use("/stations", station_router)
 async function start_database() {
   debugLog("Connecting to database")
 
-  if (process.env.MONGO_URI === undefined) {
-    errorLog("MONGO_URI is not defined, please define it via the MONGO_URI environment variable or in the .env file")
-    process.exit(1)
-  }
+  const mongo_uri = await get_mongo_uri()
 
-  await mongoose.connect(process.env.MONGO_URI)
+  debugLog("Database URI: " + mongo_uri)
+
+  await mongoose.connect(mongo_uri)
 
   try {
     debugLog("Initializing the database")
@@ -72,6 +75,41 @@ async function start_database() {
     }
   } catch (error) {
     errorLog(error)
+  }
+}
+
+/**
+ * Will try and retrieve the database URI from the MONGO_URI environment variable if it is defined, otherwise it will try to read the MONGO_URI_FILE environment variable and return the contents of the file.
+ *
+ * @returns The database URI
+ */
+const get_mongo_uri = async (): Promise<string> => {
+  if (process.env.MONGO_URI !== undefined) {
+    //If the MONGO_URI is defined, return it
+    return process.env.MONGO_URI
+  } else {
+    debugLog(
+      "MONGO_URI is not defined, trying to read it from the MONGO_URI_FILE environment variable"
+    )
+    //If the MONGO_URI is not defined, try to read it from the MONGO_URI_FILE environment variable
+    if (process.env.MONGO_URI_FILE !== undefined) {
+      try {
+        return await fs.promises.readFile(process.env.MONGO_URI_FILE, {
+          encoding: "utf-8",
+        })
+      } catch (error) {
+        errorLog(
+          "MONGO_URI_FILE is defined but the file could not be read, please check that the file exists and that the user has read permissions"
+        )
+        process.exit(1)
+      }
+    } else {
+      //If the MONGO_URI_FILE is not defined, exit the program
+      errorLog(
+        "MONGO_URI_FILE and MONGO_URI are not defined, please define it via the MONGO_URI environment variable, in the .env file or provide a path to the variable via the MONGO_URI_FILE environment variable"
+      )
+      process.exit(1)
+    }
   }
 }
 
