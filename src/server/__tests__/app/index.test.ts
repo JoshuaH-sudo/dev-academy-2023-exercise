ad0a2066e7cb9a5066e49b22af7bfc675692151c
JoshuaH-sudo <joshyboy111@protonmail.com>
2023-05-02 17:10:43 +1000

    try fixing index tests by removing step enviroment

--- src/server/__tests__/app/index.test.ts
+++ src/server/__tests__/app/index.test.ts
@@ -10,16 +10,8 @@ describe("Index", () => {
     beforeEach(async () => {
       // clears the cache
       jest.resetModules()
-
-      console.debug("Before each")
-      console.debug(process.env.MONGO_URI_FILE)
-      console.debug(process.env.MONGO_URI)
       // Reset environment variables for index test
       process.env = { ...OLD_ENV }
-
-      console.debug("after")
-      console.debug(process.env.MONGO_URI_FILE)
-      console.debug(process.env.MONGO_URI)
     })
 
     it("Will get the mongo uri from the MONGO_URI environment variable", async () => {
